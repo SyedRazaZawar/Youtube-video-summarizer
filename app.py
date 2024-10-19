2b4d25ef06bfe21b0c9c9d4b49a47bafb08aeecd
SyedRazaZawar <75534559+SyedRazaZawar@users.noreply.github.com>
2024-10-19 16:04:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from youtube_transcript_api.formatters import SRTFormatter
 # API URLs and headers for Hugging Face
 API_URL_SUMMARIZATION = "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6"
 API_URL_TTS = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
-headers = {"Authorization": "Bearer your_token_here"}  # Replace 'your_token_here' with your actual token.
+headers = {"Authorization": "Bearer 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"}  # Replace 'your_token_here' with your actual token.
 
 # Streamlit webpage configuration
 st.set_page_config(page_title="YouTube Caption, Summarizer, and TTS", layout="wide")
